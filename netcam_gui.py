200be3ba9ec6ac3715a37d8512188d881db34e47
Christopher Hayes <Christopher.Hayes@solutech.tech>
2017-11-20 01:15:57 -0500

    netcam_gui.py now uses the mjpeg stream for a smoother demo feed

--- netcam_gui.py
+++ netcam_gui.py
@@ -1,86 +1,38 @@
-import gi, requests, time, socket, thread
+import gi, requests, thread
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gdk, GdkPixbuf, GObject
-from requests.auth import HTTPBasicAuth
-
-def getNetcam(num):
-    print(num)
-    response = requests.get('http://10.68.68.22:80/goform/snapshot?n=' + str(num), auth=HTTPBasicAuth('admin', 'admin'), headers={'User-Agent':'Dalvik/2.1.0 (Linux; U; Android 6.0.1; SM-N910V Build/MMB29M)', 'Connection':'Keep-Alive', 'Accept-Lanuage':'en-US', 'Accept-Encoding':'gzip, deflate', 'Referer':'http://10.68.68.22/apcam/for-android/CamPreview.asp?SeedonkServerIp=server.seedonk.com&PartnerId=Belkin&TZ=America/Detroit&LanguageId=0', 'Accept':'image/webp,image/apng,image/*,*/*;q=0.8', 'X-Requested-With':'com.belkin.android.androidbelkinnetcam'})
-    f = open('newest_image.jpeg', 'w+')
-    f.write(response.content)
-    f.close()
+from gi.repository import Gtk, GdkPixbuf
 
 class MyWindow(Gtk.Window):
-
     def __init__(self):
         Gtk.Window.__init__(self, title="NetCam Preview")
-
-        self.hbox = Gtk.HBox()
-        self.hbox.show()
-        self.add(self.hbox)
-
         self.image = Gtk.Image()
         self.image.set_from_file("newest_image.jpeg")
         self.image.show()
-        self.hbox.add(self.image)
-
-        #self.image2 = Gtk.Image()
-        #self.image2.set_from_file("newest_image.jpeg")
-        #self.image2.show()
-        #self.hbox.add(self.image2)
-
-        #self.image_counter = 1
-        #GObject.timeout_add(250, self.update_image);
-
-    def update_image(self):
-        #getNetcam(self.image_counter)
-        #self.image_counter += 1
-        self.image.set_from_file("newest_image.jpg")
-        self.image.show()
-
-        #getNetcam(1)
-        #self.image2.set_from_file("newest_image.jpeg")
-        #self.image2.show()
-
-        return True
-
+        self.add(self.image)
+    # stream video data via mjpeg
     def stream(self):
         s = requests.session()
         stream = s.get('http://admin:admin@10.68.68.22:80/goform/video', stream=True)
         if stream.ok:
-            x = True
-            # get header
+            # get header; stream, so this loops until stream or app closes
             for chunk in stream.iter_content(chunk_size=77):
+                if not m_running and stream.ok:
+                    break
                 # get jpeg data
                 size = int(chunk[-10:-4])
-                print(size)
-                snap = open('newest_image.jpg', 'wb')
                 for image_data in stream.iter_content(chunk_size=size + 2):
-                    snap.write(image_data[:-2])
-                    loader = Gdk.PixbufLoader('jpeg')
+                    loader = GdkPixbuf.PixbufLoader()
                     loader.write(image_data[:-2])
-                    loader.close()
-                    # load pixbuf
-                    pixbuf = loader.get_pixbuf()
                     # set image
-                    gtk_image_set_from_pixbuf(self.image, pixbuf)
+                    self.image.set_from_pixbuf(loader.get_pixbuf())
+                    loader.close()
                     break
-                snap.close()
-                x = False
 
-
-#response = requests.get('http://10.68.68.22:80/goform/getSystemSettings?systemModel&systemVersion', headers={'User-Agent':'Dalvik/2.1.0 (Linux; U; Android 6.0.1; SM-N910V Build/MMB29M)', 'Connection':'Keep-Alive'})
-##print(response.content)
-response = requests.get('http://admin:admin@10.68.68.22:80/goform/getSystemSettings?systemModel&systemVersion', headers={'User-Agent':'Dalvik/2.1.0 (Linux; U; Android 6.0.1; SM-N910V Build/MMB29M)', 'Connection':'Keep-Alive'})
-print(response.status_code)
-print(response.content)
-#response = requests.post('http://admin:admin@10.68.68.22:80/goform/video', headers={'User-Agent':'Dalvik/2.1.0 (Linux; U; Android 6.0.1; SM-N910V Build/MMB29M)'})
-#print(response.status_code)
-#print(response.content)
-
-print("launch gtk")
+m_running = True
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
+# stream data on thread separate from gui thread
 thread.start_new_thread(win.stream, ())
 Gtk.main()
+m_running = False
